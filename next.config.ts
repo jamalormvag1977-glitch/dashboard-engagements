583f5b79049c29c188b4efb1a84689da8d058726
Z User <z@container>
2026-05-11 10:57:06 +0000

    1199891c-3cfc-4a72-9547-261c6b21251d

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  allowedDevOrigins: [
+    'preview-9d9f03eb-ada4-452b-9f38-1f4bf90e452d.space.chatglm.site',
+    '.space.chatglm.site',
+  ],
 };
 
 export default nextConfig;
